359ac5060f7c957ca01b54e69765631974c7c4c6
Marius <c_marian98@yahoo.com>
2018-03-21 15:06:39 +0200

    alphabet and states are strings instead of chars and i create a class for DFA

--- main.cpp
+++ main.cpp
@@ -1,70 +1,121 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 
 #define mp(x,y) make_pair(x,y)
 
 using namespace std;
 
-
-//check if a word belong to an automata
-bool checkWord(const string &word, const string &states, const string &alphabet,
-               map<pair<char, char>, char> &transition, const char &initialState, const string &finalStates)
+class DFA
 {
-    char state = initialState;
-    for(char c : word)
-        state = transition[mp(state, c)];
-    return finalStates.find(state) != string::npos;
+    vector<string> states, alphabet, finalStates;
+    map<pair<string, string>, string> transition;
+    string initialState;
+
+
+public:
+    DFA(const vector<string> &_states, const vector<string> &_alphabet,const map<pair<string, string>, string> &_transition,
+        const string &_initialState, const vector<string> &_finalStates)
+    {
+        states = _states;
+        alphabet = _alphabet;
+        transition = _transition;
+        initialState = _initialState;
+        finalStates = _finalStates;
+    }
+
+    bool checkWord(const vector<string> &word)
+    {
+        string state = initialState;
+        for(string c : word)
+            state = transition[mp(state, c)];
+        return find(finalStates.begin(), finalStates.end(), state) != finalStates.end();
+    }
+
+    void check()
+    {
+        cout<<"hello world! :D";
+    }
+};
+
+vector<string> split(string s)
+{
+    vector<string> rez;
+    string buff;
+
+    for(char c : s)
+    {
+        if(c != ' ')
+            buff += c;
+
+        else if(buff != "")
+        {
+            rez.push_back(buff);
+            buff = "";
+        }
+    }
+
+    if(buff != "")
+        rez.push_back(buff);
+
+    return rez;
 }
 
 
-void example1()
+DFA readDFA(string fileName)
 {
-    string states = "spqr";
-    string alphabet = "ab";
-    map<pair<char,char>, char> t;
-    t[mp('s','a')] = 'p';
-    t[mp('s','b')] = 's';
-    t[mp('p','a')] = 'p';
-    t[mp('p','b')] = 'q';
-    t[mp('q','a')] = 'r';
-    t[mp('q','b')] = 's';
-    t[mp('r','a')] = 'r';
-    t[mp('r','b')] = 'r';
-    char initialState = 's';
-    string finalStates = "r";
+    /*
+     * read format:
+     * line 1: states
+     * line 2: alphabet
+     * line 3: initial state
+     * line 4: final states
+     * rest of lines: transition function
+    */
 
-    string word = "baabbbaaababbababb";
+    ifstream fin(fileName);
+    vector<string> states, alphabet, finalStates, word;
+    string initialState, line;
+    map<pair<string, string>, string> t;
 
-    bool rez = checkWord(word,states,alphabet,t,initialState,finalStates);
 
-    cout<<rez;
+    getline(fin, line);
+    states = split(line);
+
+    getline(fin, line);
+    alphabet = split(line);
+
+    getline(fin, initialState);
+
+    getline(fin, line);
+    finalStates = split(line);
+
+    string s1, s2, s3;
+    while(!fin.eof())
+    {
+        fin >> s1 >> s2 >> s3;
+        t[mp(s1,s2)] = s3;
+    }
+
+    fin.close();
+
+    DFA d(states,alphabet,t,initialState,finalStates);
+
+    return d;
 }
 
-void example2()
-{
-    string states = "012";
-    string alphabet = "ab";
-    map<pair<char,char>, char> t;
-    t[mp('0','a')] = '1';
-    t[mp('0','b')] = '0';
-    t[mp('1','a')] = '2';
-    t[mp('1','b')] = '0';
-    t[mp('2','a')] = '2';
-    t[mp('2','b')] = '2';
-    char initialState = '0';
-    string finalStates = "01";
-
-    string word = "bbababbba";
-
-    bool rez = checkWord(word,states,alphabet,t,initialState,finalStates);
-
-    cout<<rez;
-}
 
 int main()
 {
-    example2();
+    DFA D = readDFA("dfa1.txt");
+
+    string word = "";
+
+    cout<<D.checkWord(split(word));
+
+
     return 0;
 }
